2dcdead7a843067ea27ba01328fb58be919fd9e3
落小梅 <425605679@qq.com>
2022-01-25 01:33:24 +0800

    wip: workspace

--- vc-ant/VcAntForm/type.ts
+++ vc-ant/VcAntForm/type.ts
@@ -1,48 +1,62 @@
-// import { FormProps, InputProps, SelectProps } from 'ant-design-vue'
-// import { Ref } from 'vue'
-//
-// // export type VcAntFormComponentName =
-// //   | 'AInput'
-// //   | 'ASelect'
-// //   | 'ARadio'
-// //   | 'ASwitch'
-// //   | 'ACheckbox'
-// //   | 'ADatePicker'
-// //   | 'Slot'
-//
-// export interface VcAntRunTimeComponentType {
-//   AInput: InputProps
-//   ASelect: SelectProps
+import type {
+  FormProps,
+  SelectProps,
+  CheckboxProps,
+  SwitchProps,
+  TreeSelectProps,
+  InputNumberProps,
+  DatePickerProps,
+  TimePickerProps,
+  TimeRangePickerProps,
+  RateProps,
+  TransferProps
+} from 'ant-design-vue'
+import { InputProps } from 'ant-design-vue/es/input/inputProps'
+import { VNode } from 'vue'
+
+export interface VcAntRunTimeComponentType {
+  AInput: InputProps
+  AInputNumber: InputNumberProps
+  ASelect: SelectProps
+  ACheckbox: CheckboxProps
+  ASwitch: SwitchProps
+  ATreeSelect: TreeSelectProps
+  ADatePicker: DatePickerProps
+  ATimePicker: TimePickerProps
+  ATimeRange: TimeRangePickerProps
+  ARate: RateProps
+  ATransfer: TransferProps
+  Slots: VNode
+}
+
+export type VcAntFormComponentName = keyof VcAntRunTimeComponentType
+
+export type ComponentProps<T> = T extends VcAntFormComponentName
+  ? VcAntRunTimeComponentType[T]
+  : never
+
+export interface AntFormSchema {
+  field: string
+  component: VcAntFormComponentName
+  formItemProps: FormProps
+  componentProps: ComponentProps<VcAntFormComponentName>
+}
+
+// export interface VcNaiveFormProps extends FormProps {
+//   model?: Recordable
+//   schemas?: AntFormSchema[]
+//   gridProps?: Partial<GridProps>
 // }
-//
-// export type VcAntFormComponentName = keyof VcAntRunTimeComponentType
-//
-// export type ComponentProps<T> = T extends VcAntFormComponentName
-//   ? VcAntRunTimeComponentType[T]
-//   : never
-//
-// export interface AntFormSchema {
-//   field: string
-//   component: VcAntFormComponentName
-//   formItemProps: FormProps
-//   componentProps: ComponentProps<VcAntFormComponentName>
+
+// export interface VcNaiveFormExpose {
+//   values: Ref<Recordable>
+//   updFormProps: (formProps?: Partial<VcNaiveFormProps>) => void
+//   getFormValue: () => Recordable
+//   updFormValue: (updModel: Recordable) => void
+//   validate: (args?: any) => Promise<any>
+//   restoreValidation: () => void
 // }
-//
-// // export interface VcNaiveFormProps extends FormProps {
-// //   model?: Recordable
-// //   schemas?: AntFormSchema[]
-// //   gridProps?: Partial<GridProps>
-// // }
-//
-// // export interface VcNaiveFormExpose {
-// //   values: Ref<Recordable>
-// //   updFormProps: (formProps?: Partial<VcNaiveFormProps>) => void
-// //   getFormValue: () => Recordable
-// //   updFormValue: (updModel: Recordable) => void
-// //   validate: (args?: any) => Promise<any>
-// //   restoreValidation: () => void
-// // }
-//
-// // export interface VcNaiveFormEmit {
-// //   (e: 'register-form', expose?: VcNaiveFormExpose): void
-// // }
+
+// export interface VcNaiveFormEmit {
+//   (e: 'register-form', expose?: VcNaiveFormExpose): void
+// }
